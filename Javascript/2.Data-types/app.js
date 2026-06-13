6c5535110957cde2019dab61c8fb46313f8fe0d1
adilmalik01 <malikadil5679@gmail.com>
2026-06-13 17:11:18 +0500

    add: Initialize app.js and index.html with variable examples and structure

--- Javascript/2.Data-types/app.js
+++ Javascript/2.Data-types/app.js
@@ -1,4 +1,20 @@
+// String 
+// console.log("khasdbljhdsaba hjlakbcha873q9436t9432thjvdkagsvd1234567890");
+// console.log('kajsndkjasda');
+
+// Number 
+// console.log(1234567890);
+
+// Bolean 
+// console.log(true);
+// console.log(false);
+
+// null 
+// console.log(null);
+
+// undefined
+// console.log(undefined);
 
 
-// console.log("Hlelo kabsdhiabdka")
-// alert("Hello World")
+// Array 
+// Object 
---
+++ Javascript/3.Variables/app.js
@@ -0,0 +1,80 @@
+// // console.log("Ali");
+// // console.log("Ali");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+// // console.log("Adil");
+
+
+
+
+
+// var myName = "Ali"
+
+
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+
+
+// myName = "Adil"
+
+
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+
+
+// var myName2 = "Hamza"
+// console.log(myName2);
+
+
+
+
+
+
+
+
+
+
+// var 
+// var num1 = 10
+// var num1 = 20
+// console.log(num1);
+
+// let 
+// let num2 = 20
+// let num2 = 50
+// console.log(num2);
+
+// // const 
+const num3 = 30
+num3 = 50
+console.log(num3);
